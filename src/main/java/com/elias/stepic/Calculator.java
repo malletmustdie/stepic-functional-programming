0a944a63213a0ab03f1f3baa8c69f53b3fad5055
Elias <mallet322@gmail.com>
2022-01-30 14:40:02 +0300

    1. The sum of two numbers.

---
+++ src/main/java/com/elias/stepic/Calculator.java
@@ -0,0 +1,16 @@
+package com.elias.stepic;
+
+/**
+ * The sum and product of two numbers.
+ */
+public class Calculator {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+
+}
---
+++ src/test/java/com/elias/stepic/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.elias.stepic;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void whenSum10And8Then18() {
+        int a = 10;
+        int b = 8;
+        int expected = 18;
+        int actual = Calculator.sum(a, b);
+        Assert.assertThat(expected, Matchers.is(actual));
+    }
+
+    @Test
+    public void whenMult10And8Then80() {
+        int a = 10;
+        int b = 8;
+        int expected = 80;
+        int actual = Calculator.mult(a, b);
+        Assert.assertThat(expected, Matchers.is(actual));
+    }
+
+}
