bbf2c2aca84f0d3ae38725170d404255e511173b
Elias <mallet322@gmail.com>
2022-01-30 16:04:21 +0300

    2. The min of two numbers.

--- src/main/java/com/elias/stepic/Operator.java
+++ src/main/java/com/elias/stepic/Operator.java
@@ -4,7 +4,7 @@ import java.util.function.BiFunction;
 
 public class Operator {
 
-    private static final BiFunction<Integer, Integer, Integer> BI_FUNCTION = Math::min;
+    private static final BiFunction<Integer, Integer, Integer> BI_FUNCTION = (a, b) -> a < b ? a : b;
 
     public static Integer getMinimum(Integer i1, Integer i2) {
         return BI_FUNCTION.apply(i1, i2);
